a3da96b2097e1af626c56161ac945946f3489674
skywyjk <121789820+skywyjk@users.noreply.github.com>
2024-08-02 16:53:33 +0800

    Update Alist.bat

--- Alist.bat
+++ Alist.bat
@@ -1,4 +1,4 @@
-@set iasver=0.0.3
+@set iasver=0.0.4
 @setlocal DisableDelayedExpansion
 @echo off
 :MainMenu
@@ -18,22 +18,30 @@ echo:               [3] 结束Alist服务
 echo:               [4] 打开Alist网页
 echo:               [5] 打开Alist管理
 echo:               [6] 设置Alist密码
+echo:               [7] 检测Alist运行 
+echo:               [8] 查看Alist项目
+echo:               [9] 查看Alist官网
 echo:               [0] 退出
 echo:            ___________________________________________________
 echo:         
 echo:         制作^ By sky
 echo:         
-echo: 输入键盘上的菜单选项 [1,2,3,4,5,6,0]"
-choice /C:1234560 /N
-set _erl=%errorlevel%
-
-if %_erl%==7 exit /b
-if %_erl%==6 alist admin set admin
-if %_erl%==5 start http://localhost:5244/@manage
-if %_erl%==4 start http://localhost:5244
-if %_erl%==3 alist.exe stop
-if %_erl%==2 alist.exe restart
-if %_erl%==1 alist.exe start
+echo:用键盘输入完后,请按下回车键！
+echo:输入键盘上的菜单选项 [1,2,3,4,5,6,7,8,9,0]
+echo: 
+set choice=0
+set /p choice=选择
+if "%choice%"=="0" exit /b
+if "%choice%"=="1" alist.exe start
+if "%choice%"=="2" alist.exe restart
+if "%choice%"=="3" alist.exe stop
+if "%choice%"=="4" start http://localhost:5244
+if "%choice%"=="5" start http://localhost:5244/@manage
+if "%choice%"=="6" alist admin set admin
+if "%choice%"=="7" ping http://localhost:5244
+if "%choice%"=="8" start https://github.com/alist-org/alist
+if "%choice%"=="9" start https://alist.nn.ci/zh/
 ping 127.0.0.1 -n 2 > nul
-goto:MainMenu
+goto :MainMenu
 
+:: 在此留下空行
