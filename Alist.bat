9fbf7706e8b1a2dfcdf742396e0b469231421ea7
skywyjk <121789820+skywyjk@users.noreply.github.com>
2024-08-13 18:47:45 +0800

    更新 Alist.bat

--- Alist.bat
+++ Alist.bat
@@ -1,9 +1,9 @@
-@set iasver=0.0.7
+@set version=0.0.8
 @setlocal DisableDelayedExpansion
 @echo off
 :MainMenu
 cls
-title  Alist脚本服务_sky制作 v%iasver%
+title  Alist脚本服务_sky制作 v%version%
 echo:
 echo:
 echo:
@@ -20,28 +20,27 @@ echo:               [5] 打开Alist管理
 echo:               [6] 设置Alist密码
 echo:               [7] 查看Alist项目 
 echo:               [8] 进入Alist官网
+echo:               [9] 查询本项目版本
 echo:               [0] 退出
 echo:            ___________________________________________________
 echo:         
 echo:         制作^ By sky
 echo:         
 echo:用键盘输入完后,请按下回车键！
-echo:输入键盘上的菜单选项 [1,2,3,4,5,6,7,8,0]
+echo:输入键盘上的菜单选项 [1,2,3,4,5,6,7,8,9,0]
 echo: 
 set choice=0
 set /p choice=选择
-if "%choice%"=="0" taskkill -f -t -im alist.exe
-if "%choice%"=="0" rmdir /s /q data
-if "%choice%"=="0" rmdir /s /q daemon
-if "%choice%"=="0" exit /b
-if "%choice%"=="1" alist.exe start
-if "%choice%"=="2" alist.exe restart
-if "%choice%"=="3" alist.exe stop
-if "%choice%"=="4" start http://localhost:5244
-if "%choice%"=="5" start http://localhost:5244/@manage
+if "%choice%"=="0" goto :exit
+if "%choice%"=="1" goto :start
+if "%choice%"=="2" goto :restart
+if "%choice%"=="3" goto :stop
+if "%choice%"=="4" goto :web
+if "%choice%"=="5" goto :admin
 if "%choice%"=="6" goto :passworld
-if "%choice%"=="7" start https://github.com/alist-org/alist
-if "%choice%"=="8" start https://alist.nn.ci/zh/
+if "%choice%"=="7" goto :github-alist
+if "%choice%"=="8" goto :alist
+if "%choice%"=="9" goto :version
 ping 127.0.0.1 -n 2 > nul
 goto :MainMenu
 
@@ -65,10 +64,20 @@ echo:输入键盘上的菜单选项 [1,2,3,0]
 echo: 
 set choice=0
 set /p choice=选择
-if "%choice%"=="1" alist admin random
+if "%choice%"=="1" goto :random
 if "%choice%"=="2" goto :newpassworld
-if "%choice%"=="3" alist admin set admin
+if "%choice%"=="3" goto :passworld-admin
 if "%choice%"=="0" goto :MainMenu
+ping 127.0.0.1 -n 3 > nul
+goto :MainMenu
+
+:random
+alist.exe admin random
+ping 127.0.0.1 -n 6 > nul
+goto :MainMenu
+
+:passworld-admin
+alist.exe admin set admin
 ping 127.0.0.1 -n 2 > nul
 goto :MainMenu
 
@@ -76,9 +85,302 @@ goto :MainMenu
 echo:请输入你需要设置的密码
 set choice=0
 set /p new passworld=
-alist admin set "%new passworld%"
+alist.exe admin set "%new passworld%"
 ping 127.0.0.1 -n 2 > nul
 goto :MainMenu
 
+:exit
+cls
+echo:
+echo:
+echo:
+echo:                  是否退出本程序（Y/N）
+echo:            ___________________________________________________ 
+echo:                                                                            
+echo:               [Y] 退出本程序
+echo:               [N] 返回主菜单
+echo:            ___________________________________________________ 
+echo:         
+echo:输入键盘上的菜单选项 [Y,N]
+set choice=0
+set /p choice=选择
+if "%choice%"=="Y" taskkill -f -t -im alist.exe
+if "%choice%"=="Y" rmdir /s /q data
+if "%choice%"=="Y" rmdir /s /q daemon
+if "%choice%"=="Y" exit /b
+if "%choice%"=="N" goto :MainMenu
+if "%choice%"=="y" taskkill -f -t -im alist.exe
+if "%choice%"=="y" rmdir /s /q data
+if "%choice%"=="y" rmdir /s /q daemon
+if "%choice%"=="y" exit /b
+if "%choice%"=="n" goto :MainMenu
+ping 127.0.0.1 -n 2 > nul
+goto :MainMenu
 
+:start
+cls
+echo:
+echo:
+echo:
+echo:                  是否启动程序（Y/N）
+echo:            ___________________________________________________ 
+echo:                                                                            
+echo:               [Y] 启动程序
+echo:               [N] 返回主菜单
+echo:            ___________________________________________________ 
+echo:         
+echo:输入键盘上的菜单选项 [Y,N]
+set choice=0
+set /p choice=选择
+if "%choice%"=="Y" alist.exe start
+if "%choice%"=="N" goto :MainMenu
+if "%choice%"=="y" alist.exe start
+if "%choice%"=="n" goto :MainMenu
+ping 127.0.0.1 -n 2 > nul
+goto :MainMenu
 
+:stop
+cls
+echo:
+echo:
+echo:
+echo:                  是否结束程序（Y/N）
+echo:            ___________________________________________________ 
+echo:                                                                            
+echo:               [Y] 结束程序
+echo:               [N] 返回主菜单
+echo:            ___________________________________________________ 
+echo:         
+echo:输入键盘上的菜单选项 [Y,N]
+set choice=0
+set /p choice=选择
+if "%choice%"=="Y" alist.exe stop
+if "%choice%"=="N" goto :MainMenu
+if "%choice%"=="y" alist.exe stop
+if "%choice%"=="n" goto :MainMenu
+ping 127.0.0.1 -n 2 > nul
+goto :MainMenu
+
+:restart
+cls
+echo:
+echo:
+echo:
+echo:                  是否重启程序（Y/N）
+echo:            ___________________________________________________ 
+echo:                                                                            
+echo:               [Y] 重启程序
+echo:               [N] 返回主菜单
+echo:            ___________________________________________________ 
+echo:         
+echo:输入键盘上的菜单选项 [Y,N]
+set choice=0
+set /p choice=选择
+if "%choice%"=="Y" alist.exe restart
+if "%choice%"=="N"  goto :MainMenu
+if "%choice%"=="y" alist.exe restart
+if "%choice%"=="n"  goto :MainMenu
+ping 127.0.0.1 -n 2 > nul
+goto :MainMenu
+
+:web
+cls
+echo:
+echo:
+echo:
+echo:                  是否打开Alist网页（Y/N）
+echo:            ___________________________________________________ 
+echo:                                                                            
+echo:               [Y] 打开Alist网页
+echo:               [N] 返回主菜单
+echo:            ___________________________________________________ 
+echo:         
+echo:输入键盘上的菜单选项 [Y,N]
+set choice=0
+set /p choice=选择
+if "%choice%"=="Y" start http://localhost:5244
+if "%choice%"=="N"  goto :MainMenu
+if "%choice%"=="y" start http://localhost:5244
+if "%choice%"=="n"  goto :MainMenu
+ping 127.0.0.1 -n 2 > nul
+goto :MainMenu
+
+:admin
+cls
+echo:
+echo:
+echo:
+echo:                  是否进入Alist管理界面（Y/N）
+echo:            ___________________________________________________ 
+echo:                                                                            
+echo:               [Y] 进入Alist管理界面
+echo:               [N] 返回主菜单
+echo:            ___________________________________________________ 
+echo:         
+echo:输入键盘上的菜单选项 [Y,N]
+set choice=0
+set /p choice=选择
+if "%choice%"=="Y" start http://localhost:5244/@manage
+if "%choice%"=="N"  goto :MainMenu
+if "%choice%"=="y" start http://localhost:5244/@manage
+if "%choice%"=="n"  goto :MainMenu
+ping 127.0.0.1 -n 2 > nul
+goto :MainMenu
+
+:github-alist
+cls
+echo:
+echo:
+echo:
+echo:             是否查看Github上的Alist项目（Y/N）
+echo:            ___________________________________________________ 
+echo:                                                                            
+echo:               [Y] 查看
+echo:               [N] 返回主菜单
+echo:            ___________________________________________________ 
+echo:         
+echo:输入键盘上的菜单选项 [Y,N]
+set choice=0
+set /p choice=选择
+if "%choice%"=="Y" start https://github.com/alist-org/alist
+if "%choice%"=="N"  goto :MainMenu
+if "%choice%"=="y" start https://github.com/alist-org/alist
+if "%choice%"=="n"  goto :MainMenu
+ping 127.0.0.1 -n 2 > nul
+goto :MainMenu
+
+:alist
+cls
+echo:
+echo:
+echo:
+echo:                  是否进入Alist官网
+echo:            ___________________________________________________ 
+echo:                                                                            
+echo:               [1] 进入中文官网
+echo:               [2] 进入英文官网
+echo:               [0] 返回主菜单
+echo:            ___________________________________________________ 
+echo:         
+echo:输入键盘上的菜单选项 [1,2,0]
+set choice=0
+set /p choice=选择
+if "%choice%"=="1" start https://alist.nn.ci/zh/
+if "%choice%"=="2" start https://alist.nn.ci/
+if "%choice%"=="0"  goto :MainMenu
+ping 127.0.0.1 -n 2 > nul
+goto :MainMenu
+
+:version
+cls
+echo:
+echo:
+echo:
+echo:
+echo:               经过查询，本项目版本为v%version%
+echo:               是否获取本项目的最新版本？
+echo:            ___________________________________________________ 
+echo:                                                                            
+echo:               [Y] 获取
+echo:               [N] 返回主菜单
+echo:            ___________________________________________________ 
+echo:         
+echo:输入键盘上的菜单选项 [Y,N]
+set choice=0
+set /p choice=选择
+if "%choice%"=="Y" goto :update
+if "%choice%"=="N"  goto :MainMenu
+if "%choice%"=="y" goto :update
+if "%choice%"=="n"  goto :MainMenu
+ping 127.0.0.1 -n 2 > nul
+goto :MainMenu
+
+:update-github
+start https://github.com/skywyjk/Alist.bat
+ping 127.0.0.1 -n 2 > nul
+goto :MainMenu
+
+:update-gitee
+start https://gitee.com/skywyjk/alist.bat
+ping 127.0.0.1 -n 2 > nul
+goto :MainMenu
+
+:update-github-tyro
+start https://github.com/skywyjk/Alist.bat/releases
+ping 127.0.0.1 -n 2 > nul
+goto :MainMenu
+
+:update-gitee-tyro
+start https://gitee.com/skywyjk/alist.bat/releases
+ping 127.0.0.1 -n 2 > nul
+goto :MainMenu
+
+:update
+cls
+echo:
+echo:
+echo:
+echo:                  获取本项目的最新版本
+echo:            ___________________________________________________ 
+echo:                                                                            
+echo:               [1] 进入GitHub渠道
+echo:               [2] 进入Gitee渠道
+echo:               [0] 返回主菜单
+echo:            ___________________________________________________ 
+echo:         
+echo:输入键盘上的菜单选项 [1,2,0]
+set choice=0
+set /p choice=选择
+if "%choice%"=="1" goto :questionnaire-github
+if "%choice%"=="2" goto :questionnaire-gitee
+if "%choice%"=="0"  goto :MainMenu
+ping 127.0.0.1 -n 2 > nul
+goto :MainMenu
+
+:questionnaire-github
+cls
+echo:
+echo:
+echo:
+echo:                 是否对GitHub非常熟悉
+echo:            ___________________________________________________ 
+echo:                                                                            
+echo:               [Y] 对GitHub非常熟悉
+echo:               [N] 对GitHub不太熟悉
+echo:               [0] 返回主菜单
+echo:            ___________________________________________________ 
+echo:         
+echo:输入键盘上的菜单选项 [Y,N,0]
+set choice=0
+set /p choice=选择
+if "%choice%"=="Y" goto :update-github
+if "%choice%"=="N" goto :update-github-tyro
+if "%choice%"=="y" goto :update-github
+if "%choice%"=="n" goto :update-github-tyro
+if "%choice%"=="0"  goto :MainMenu
+ping 127.0.0.1 -n 2 > nul
+goto :MainMenu
+
+:questionnaire-gitee
+cls
+echo:
+echo:
+echo:
+echo:                 是否对Gitee非常熟悉
+echo:            ___________________________________________________ 
+echo:                                                                            
+echo:               [Y] 对Gitee非常熟悉
+echo:               [N] 对Gitee不太熟悉
+echo:               [0] 返回主菜单
+echo:            ___________________________________________________ 
+echo:         
+echo:输入键盘上的菜单选项 [Y,N,0]
+set choice=0
+set /p choice=选择
+if "%choice%"=="Y" goto :update-gitee
+if "%choice%"=="N" goto :update-gitee-tyro
+if "%choice%"=="y" goto :update-github
+if "%choice%"=="n" goto :update-github-tyro
+if "%choice%"=="0"  goto :MainMenu
+ping 127.0.0.1 -n 2 > nul
+goto :MainMenu
